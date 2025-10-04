0831cef13b95093fb83b0c90c7a67c9a4536a6ba
nico-campa123 <48675751@est.ort.edu.ar>
2025-10-04 14:52:52 -0400

    deploy nacional

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# ✅ Allow all origins (for testing)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # later replace "*" with your Vercel domain
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 async def read_root():
     return {"message": "World"}
