fa9d9eb151d2e38014200f62327208bb17ae3bf4
manucorsu <142500898+manucorsu@users.noreply.github.com>
2025-10-05 17:25:58 -0300

    prueba en back

--- main.py
+++ main.py
@@ -18,6 +18,55 @@ app.add_middleware(
 )
 
 model = joblib.load("Stacking_fp.pkl")
+path = "cumulative_2025.10.04_05.21.55.csv"
+df = pd.read_csv(
+    path,
+    comment='#',      # ignore metadata lines starting with '#'
+    engine='python',  # more forgiving parser
+)
+
+def que_pruebe(content):
+    try:
+        df_init = pd.read_csv(StringIO(content.decode("utf-8")))
+    except Exception:
+        try:
+            df_init = pd.read_csv(StringIO(content.decode("latin-1")))
+        except Exception as e:
+            raise HTTPException(status_code=400, detail=f"Error parsing CSV: {e}")
+
+    # capture kepid before we drop columns
+    kepids = df_init['kepid'].tolist() if 'kepid' in df_init.columns else [None] * len(df_init)
+
+    # Drop unwanted columns
+    drop = [
+        'rowid','kepid','kepoi_name','kepler_name','koi_pdisposition',
+        'koi_score','koi_teq_err1','koi_teq_err2','koi_tce_delivname',
+        'koi_fpflag_nt','koi_fpflag_ss','koi_fpflag_co','koi_fpflag_ec'
+    ]
+    df = df_init.drop(columns=[c for c in drop if c in df_init.columns], errors="ignore")
+
+    # Replace NaN with median
+    df = df.fillna(df.median(numeric_only=True))
+
+    # Split features
+    if 'koi_disposition' in df.columns:
+        df = df.drop(columns=['koi_disposition'], errors='ignore')
+    if 'target' in df.columns:
+        df = df.drop(columns=['target'], errors='ignore')
+
+    # Drop non-numeric columns
+    non_numeric_cols = df.select_dtypes(exclude=np.number).columns.tolist()
+    X = df.drop(columns=non_numeric_cols)
+
+    # Scale features
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
+
+    # Make predictions
+    preds = model.predict(X_scaled)
+    preds_list = [int(p) for p in preds]
+    paired = [[kepids[i], preds_list[i]] for i in range(min(len(kepids), len(preds_list)))]
+    return {"predictions": paired}
 
 @app.get("/")
 async def root():
@@ -72,3 +121,11 @@ async def upload_csv(file: UploadFile = File(...)):
     preds_list = [int(p) for p in preds]
     paired = [[kepids[i], preds_list[i]] for i in range(min(len(kepids), len(preds_list)))]
     return {"predictions": paired}
+
+@app.get("/campa")
+async def root():
+    kepler = df.drop(columns=['koi_disposition'], errors='ignore')
+    non_numeric_cols = kepler.select_dtypes(exclude=np.number).columns.tolist()
+    X = kepler.drop(columns=non_numeric_cols)
+    a = model.predict(X)
+    return {"message": "Hello from FastAPI on Render!", "predictions": a}
